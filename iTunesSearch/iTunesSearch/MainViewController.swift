056115d74df6b977f824205167768e39a391fc13
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-05-12 11:33:54 +0900

    [setting] 프로젝트 초기 세팅

---
+++ iTunesSearch/iTunesSearch/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  iTunesSearch
+//
+//  Created by 서동환 on 5/12/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBackground
+    }
+
+
+}
+
